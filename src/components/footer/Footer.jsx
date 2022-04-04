ef95485d90df79d0a23df7802ac256fe955d35d4
adrikosm <adrikosm@gmail.com>
2022-04-04 22:36:32 +0200

    Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,17 +9,20 @@ import email from "../../assets/email2.png"
 const Footer = () => {
     return (
         <div className='Footer'>
-            <div className="footer_rights">
                 <p className="footer_name">
                     <span>Andreas</span> Mavropoulos</p>
+ 
+            <div className="footer_rights">
+            <p className="fotter_copyrights">© All Rights Reserved </p>
             </div>
-            <p className="fotter_copyRights">© All Rights Reserved </p>
-
 
             <div className="footer_socials">
-                <a href="https://github.com/adrikosm" target="_blank"><img src={github} alt="Github" className="footer_social_icons" /></a>
-                <a href="https://www.linkedin.com/in/andreas-mavropoulos/" target="_blank"><img src={linkedin} alt="LinkedIn" className="footer_social_icons" /></a>
-                <a href="mailto:adrikosm@gmail.com"><img src={email} alt="Email" className="footer_social_icons" /></a>
+                <a href="https://github.com/adrikosm" target="_blank">
+                    <img src={github} alt="Github" className="footer_social_icons" /></a>
+                <a href="https://www.linkedin.com/in/andreas-mavropoulos/" target="_blank">
+                    <img src={linkedin} alt="LinkedIn" className="footer_social_icons" /></a>
+                <a href="mailto:adrikosm@gmail.com">
+                    <img src={email} alt="Email" className="footer_social_icons" /></a>
 
             </div>
         </div>
